3e430e5438f07df19497af2c19afca14e35ee3ae
Đức Đào Hữu <dhd@netcompany.com>
2023-03-02 13:18:44 +0700

    Add temp file

---
+++ study-with-mentor/src/Components/ComponentTemp.js
@@ -0,0 +1,5 @@
+const ComponentTemp = () => {
+  return <div>Hello</div>;
+};
+
+export default ComponentTemp;
---
+++ study-with-mentor/src/Services/serviceTemp.js
@@ -0,0 +1,8 @@
+export const getDummyUser = () => {
+  return {
+    name: "Duc",
+    gender: "Male",
+    email: "daohuuduc.910@gmail.com",
+    role: "admin",
+  };
+};
