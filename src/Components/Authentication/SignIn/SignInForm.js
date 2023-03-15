e095d1ab64cb1951b3dfc8ad6d9e8ebad64e7a30
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2023-03-15 17:13:13 +0700

    Modify theme and sign in page

--- src/Components/Authentication/SignIn/SignInForm.js
+++ src/Components/Authentication/SignIn/SignInForm.js
@@ -1,52 +1,84 @@
 import React from "react";
 import style from "./SignInForm.module.scss";
-import { TextField, Button, Divider } from "@mui/material";
+import { Button, Divider } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { useForm } from "react-hook-form";
-import { BUTTON_LABEL, TITLE } from "../../../shared/constants";
+import { BUTTON_LABEL, PLACE_HOLDER, TITLE } from "../../../shared/constants";
 import GoogleSignInButton from "../../../shared/components/GoogleSignInButton/GoogleSignInButton";
+import CustomizedTextField from "../../../shared/components/TextField/CustomizedTextField";
 
 const SignInForm = () => {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <div className={`${style.signIn__container}`}>
-      <h1>{TITLE.GROWTH_ME}</h1>
-      <h2>{TITLE.SIGN_IN}</h2>
-      <TextField
-        fullWidth
-        required
-        id="outlined-required"
-        label={TITLE.EMAIL}
-        type="email"
-        sx={{ width: "50%" }}
-        {...register("email")}
-      />
-      <TextField
-        fullWidth
-        required
-        id="password"
-        label={TITLE.PASSWORD}
-        type="password"
-        sx={{ width: "50%" }}
-        {...register("password")}
-      />
-      <div className={`${style.forgotPassword__container}`}>
-        <a href="#">{TITLE.FORGOT_PASSWORD}</a>
-      </div>
-      <Button
-        type="submit"
-        className={`${style.signIn__button}`}
-        variant="contained"
-      >
-        {BUTTON_LABEL.LOGIN}
-      </Button>
-      <div className={`${style.divider__container}`}>
-        <Divider textAlign="center" sx={{ width: "100%" }} flexItem>
-          {TITLE.OR}
-        </Divider>
-      </div>
-      <GoogleSignInButton />
-    </div>
+    <Grid2 container className={`${style.signIn__container}`}>
+      <Grid2 md={6} className={`${style.signIn__formContainer}`}>
+        <div className={`${style.signIn__formSection}`}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className={`${style.signIn__form}`}
+          >
+            <h1 className={`${style.signIn__title}`}>{TITLE.SIGN_IN}</h1>
+            <CustomizedTextField
+              inputId="email"
+              name="Email"
+              placeholder={PLACE_HOLDER.LOGIN_EMAIL}
+              options={{ ...register("password") }}
+            />
+            <CustomizedTextField
+              inputId="password"
+              name="Password"
+              placeholder={PLACE_HOLDER.LOGIN_PASSWORD}
+              options={{ ...register("email") }}
+            />
+          </form>
+
+          <div className={`${style.forgotPassword__container}`}>
+            <a href="#" className={`${style.forgotPassword__link}`}>
+              {TITLE.FORGOT_PASSWORD}
+            </a>
+          </div>
+
+          <Button
+            fullWidth
+            type="submit"
+            className={`${style.signIn__button}`}
+            variant="contained"
+          >
+            {BUTTON_LABEL.LOGIN}
+          </Button>
+
+          <div className={`${style.divider__container}`}>
+            <Divider
+              className={`${style.divider}`}
+              textAlign="center"
+              sx={{ width: "100%" }}
+              flexItem
+            >
+              {TITLE.OR}
+            </Divider>
+          </div>
+          <GoogleSignInButton />
+
+          <div className={`${style.registerSuggestion__container}`}>
+            <span>
+              Bạn chưa có tài khoản?{" "}
+              <a href="#" className={`${style.registerSuggestion__signUpLink}`}>
+                Hãy đăng ký tại đây!
+              </a>
+            </span>
+          </div>
+        </div>
+      </Grid2>
+
+      <Grid2 md={6} maxWidth={"50%"}>
+        <img alt="background" src={require("../../../assets/image1.png")} />
+      </Grid2>
+    </Grid2>
   );
 };
 
