0f6f8579909726662222dd1e1b71d2dc5d110986
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2023-04-04 18:16:40 +0700

    Separate verify email page.

--- src/Components/Authentication/Account/Account.js
+++ src/Components/Authentication/Account/Account.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import ResetPassword from "../ResetPassword/ResetPassword";
+import SignUpSuccess from "../SignUpSuccess/SignUpSuccess";
+import { authenticationService } from "../../../Services/authenticationService";
+import { ROUTES } from "../../../shared/constants/common";
 
 const Account = () => {
+  const history = useHistory();
   const mode = new URLSearchParams(useLocation().search).get("mode");
   const oobCode = new URLSearchParams(useLocation().search).get("oobCode");
-  const renderResetPasswordPage = () => {
-    return <ResetPassword oobCode={oobCode} />;
+
+  const verifyEmail = async () => {
+    try {
+      await authenticationService.verifyEmail(oobCode);
+    } catch (error) {
+      console.log(error);
+      history.push(ROUTES.SERVER_ERROR);
+    }
   };
 
-  return (
-    <div>{mode == "resetPassword" && oobCode && renderResetPasswordPage()}</div>
-  );
+  if (mode === "verifyEmail" && oobCode) {
+    verifyEmail();
+    return <SignUpSuccess />;
+  } else if (mode === "resetPassword" && oobCode) {
+    return <ResetPassword oobCode={oobCode} />;
+  } else {
+    return <div>Not found</div>;
+  }
 };
 
 export default Account;
