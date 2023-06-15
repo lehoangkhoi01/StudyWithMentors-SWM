fad0dd3762af55c677539ecd93dba552d879353f
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2023-06-15 20:53:12 +0700

    Allow upload same file

--- src/Components/Seminars/SeminarForm/SeminarForm.js
+++ src/Components/Seminars/SeminarForm/SeminarForm.js
@@ -127,12 +127,17 @@ const SeminarForm = () => {
 
   const handleDocumentsChange = (e) => {
     const files = e.target.files;
-    if (files.length === 1) {
-      setDocuments((prev) => [...prev, files[0]]);
-    } else {
-      const newFileList = Array.from(files);
-      setDocuments((prev) => [...prev, ...newFileList]);
-    }
+    let tempDocuments = [...documents];
+    console.log(files);
+    tempDocuments = [...tempDocuments, ...Array.from(files)];
+    setDocuments(tempDocuments);
+    // console.log(files);
+    // if (files.length === 1) {
+    //   setDocuments((prev) => [...prev, files[0]]);
+    // } else {
+    //   const newFileList = Array.from(files);
+    //   setDocuments((prev) => [...prev, ...newFileList]);
+    // }
   };
 
   const handleRemoveDocuments = (index) => {
