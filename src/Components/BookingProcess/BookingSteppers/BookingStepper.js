e8a9b78c14b9e0fc31abe8fb5843a1ff60dbf409
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2023-07-12 17:33:16 +0700

    Add style for booking dialog

--- src/Components/BookingProcess/BookingSteppers/BookingStepper.js
+++ src/Components/BookingProcess/BookingSteppers/BookingStepper.js
@@ -9,14 +9,39 @@ import {
 } from "@mui/material";
 import TopicList from "../TopicList/TopicList";
 import { CustomBigCalendar } from "../../../shared/components/CustomBigCalendar/CustomBigCalendar";
+import CustomizedButton from "../../../shared/components/Button/CustomizedButton";
 import style from "./BookingStepper.module.scss";
+import { styled } from "@mui/material/styles";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 const steps = ["Chọn chủ đề", "Chọn lịch cố vấn", "Mô tả", "Ghi chú"];
 const topics = [
-  { id: 1, title: "Phỏng vấn thực tập" },
-  { id: 2, title: "Quản lý thời gian" },
-  { id: 3, title: "Hướng nghiệp" },
+  {
+    id: 1,
+    title: "Phỏng vấn thực tập",
+    field: "Kỹ năng mềm",
+    category: "Phát triển bản thân",
+  },
+  {
+    id: 2,
+    title: "Quản lý thời gian",
+    field: "Kỹ năng mềm",
+    category: "Phát triển bản thân",
+  },
+  {
+    id: 3,
+    title: "Hướng nghiệp",
+    field: "Kỹ năng mềm",
+    category: "Phát triển bản thân",
+  },
 ];
+
+const CustomStepLabel = styled(StepLabel)`
+  & > .Mui-active > svg,
+  & > .Mui-completed > svg {
+    color: #3948ab;
+  }
+`;
 
 const BookingStepper = () => {
   const [activeStep, setActiveStep] = React.useState(0);
@@ -47,8 +72,12 @@ const BookingStepper = () => {
   const renderSelectTopicUI = () => {
     return (
       <>
-        <Typography className={`${style.booking__title}`}>
-          Hãy chọn 1 chủ đề mà bạn mong muốn được tư vấn
+        <Typography
+          fontWeight={600}
+          fontSize="1.2rem"
+          className={`${style.booking__title}`}
+        >
+          Hãy chọn 1 chủ đề mà bạn mong muốn được tư vấn:
         </Typography>
         <TopicList topics={topics} setSelectedTopic={setSelectedTopic} />
       </>
@@ -88,7 +117,7 @@ const BookingStepper = () => {
             const labelProps = {};
             return (
               <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
+                <CustomStepLabel {...labelProps}>{label}</CustomStepLabel>
               </Step>
             );
           })}
@@ -108,19 +137,49 @@ const BookingStepper = () => {
             {activeStep === 0 && renderSelectTopicUI()}
             {activeStep === 1 && renderSelectSlotUI()}
 
-            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-              <Button
-                color="inherit"
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                sx={{ mr: 1 }}
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                pt: 2,
+                justifyContent: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "80%",
+                }}
               >
-                Trở về
-              </Button>
-              <Box sx={{ flex: "1 1 auto" }} />
-              <Button onClick={handleNext}>
-                {activeStep === steps.length - 1 ? "Hoàn thành" : "Tiếp tục"}
-              </Button>
+                <Grid2 container spacing={2} width="100%">
+                  {activeStep !== 0 && (
+                    <Grid2 xs={12} md={6}>
+                      <CustomizedButton
+                        onClick={handleBack}
+                        color="primary600"
+                        variant="outlined"
+                        size="small"
+                      >
+                        Trở về
+                      </CustomizedButton>
+                    </Grid2>
+                  )}
+
+                  <Grid2 xs={12} md={activeStep === 0 ? 12 : 6}>
+                    <CustomizedButton
+                      variant="contained"
+                      color="primary600"
+                      size="small"
+                      onClick={handleNext}
+                    >
+                      {activeStep === steps.length - 1
+                        ? "Hoàn thành"
+                        : "Tiếp tục"}
+                    </CustomizedButton>
+                  </Grid2>
+                </Grid2>
+              </Box>
             </Box>
           </React.Fragment>
         )}
--- src/Components/BookingProcess/TopicList/TopicList.js
+++ src/Components/BookingProcess/TopicList/TopicList.js
@@ -1,8 +1,19 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
+import { Typography, ListItemButton, List, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import style from "./TopicList.module.scss";
+
+const ListItem = styled(ListItemButton)`
+  border: 1px solid #c5cae9;
+  margin: 0.5rem;
+  border-radius: 10px;
+  padding: 1rem;
+  transition: ease-in 300ms;
+  &.Mui-selected {
+    background-color: #e8eaf6;
+    border: 3px solid #9fa8da;
+  }
+`;
 
 const TopicList = (props) => {
   const [selectedIndex, setSelectedIndex] = React.useState(1);
@@ -16,13 +27,29 @@ const TopicList = (props) => {
     <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
       <List component="nav" aria-label="secondary mailbox folder">
         {props.topics?.map((topic) => (
-          <ListItemButton
+          <ListItem
             key={topic.id}
             selected={selectedIndex === topic.id}
             onClick={(event) => handleListItemClick(event, topic)}
           >
-            <ListItemText primary={topic.title} />
-          </ListItemButton>
+            <div>
+              <Typography
+                className={`${style.booking__topicList__title}`}
+                variant="h6"
+              >
+                {topic.title}
+              </Typography>
+              <div>
+                <span className={style.booking__topicList__detail}>
+                  Nhóm: {topic.field}
+                </span>{" "}
+                |{" "}
+                <span className={style.booking__topicList__detail}>
+                  Lĩnh vực: {topic.category}
+                </span>
+              </div>
+            </div>
+          </ListItem>
         ))}
       </List>
     </Box>
