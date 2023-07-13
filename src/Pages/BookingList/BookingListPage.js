40d95c8d479ff54a02d9b1a3bb98d78f21206395
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2023-07-13 15:53:07 +0700

    Add booking list page

---
+++ src/Pages/BookingList/BookingListPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BookingList from "../../Components/BookingList/BookingList";
+
+export const BookingListPage = () => {
+  return (
+    <div>
+      <BookingList />
+    </div>
+  );
+};
