b99830aad4ba222130d9a7f28ebcae36576e3be5
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2023-07-25 21:04:59 +0700

    Fix authorize routes

--- src/Layout/AdminStaffRoutes.js
+++ src/Layout/AdminStaffRoutes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router";
 import NotFoundPage from "../Components/Error/NotFound/NotFoundPage";
 import { ROUTES } from "../shared/constants/navigation";
 import FeedbackOverviewPage from "../Pages/EventFeedback/FeedbackOverviewPage";
+import MentorListAdminPage from "../Pages/Mentor/MentorListAdminPage";
 
 const AdminStaffRoutes = () => {
   return (
@@ -11,6 +12,10 @@ const AdminStaffRoutes = () => {
         <Route
           path={ROUTES.FEEDBACK_OVERVIEW}
           component={FeedbackOverviewPage}
+        />
+        <Route
+          path={ROUTES.ADMIN_MENTOR_LIST}
+          component={MentorListAdminPage}
         />
         <Route component={NotFoundPage} />
       </Switch>
