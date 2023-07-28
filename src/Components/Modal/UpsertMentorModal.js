1534658d5c09ce9cee8137e020d849e27a59d76e
unknown <daohuuduc.910@gmail.com>
2023-07-29 00:43:04 +0700

    Fix bug when create the existed email

--- src/Components/Modal/UpsertMentorModal.js
+++ src/Components/Modal/UpsertMentorModal.js
@@ -55,12 +55,6 @@ const UpsertMentorModal = (props) => {
 
     try {
       setLoading(true);
-
-      if (type === MODAL_TYPE.ADD && checkExistedEmail()) {
-        setIsExistedEmail(true);
-        return;
-      }
-
       setIsExistedEmail(false);
 
       if (type === MODAL_TYPE.EDIT) {
@@ -83,24 +77,18 @@ const UpsertMentorModal = (props) => {
       }
       props.onCloseModal();
     } catch (error) {
-      setNotification({
-        isOpen: true,
-        type: "error",
-        message: ERROR_MESSAGES.COMMON_ERROR,
-      });
+      if (error.data.includes("already exists")) {
+        setIsExistedEmail(true);
+      } else {
+        setNotification({
+          isOpen: true,
+          type: "error",
+          message: ERROR_MESSAGES.COMMON_ERROR,
+        });
+      }
     } finally {
       setLoading(false);
     }
-  };
-
-  const checkExistedEmail = () => {
-    const index = props.allMentors
-      .map((item) => item.email)
-      .indexOf(getValues("email").trim());
-
-    if (index > -1) return true;
-
-    return false;
   };
 
   const validatePhoneNum = (phoneNum) => {
