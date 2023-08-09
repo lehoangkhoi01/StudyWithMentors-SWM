d0c205bfcf72f554c770f5b1cddd5d43b16ebe99
unknown <daohuuduc.910@gmail.com>
2023-08-09 22:27:04 +0700

    Complete about us page

--- src/Pages/AboutUs/AboutUsPage.js
+++ src/Pages/AboutUs/AboutUsPage.js
@@ -1,8 +1,7 @@
-import { Divider, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { intro, solution } from "./content";
 import style from "./AboutUsPage.module.scss";
 import React from "react";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 const AboutUsPage = () => {
   return (
@@ -10,50 +9,114 @@ const AboutUsPage = () => {
       <div>
         <Typography
           fontWeight={600}
-          variant="h3"
+          variant="h1"
           color="#283493"
           textAlign="center"
-          marginBottom={2}
+          className={`${style.about__title}`}
         >
           Growth Me
         </Typography>
-      </div>
-      <div className={`${style.about__intro}`}>
-        <Typography variant="body1">{intro.par1}</Typography>
-        <Typography variant="body1">{intro.par2}</Typography>
-        <Typography variant="body1">{intro.par3}</Typography>
-        <Typography>
-          <div>{`- Trao đổi, giải đáp thắc mắc sau ngay cả sau khi sự kiện đã kết thúc.`}</div>
-          <div>{`- Kết nối giữa mentor - là những diễn giả với sinh viên Đại học FPT.`}</div>
+        <Typography
+          fontWeight={500}
+          variant="h5"
+          color="#283493"
+          textAlign="center"
+          marginBottom={2}
+          className={`${style.about__subTitle}`}
+        >
+          Phát triển bản thân, phát triển cộng đồng
         </Typography>
       </div>
-      <Grid2 container justifyContent="center" gap={2} marginTop={3}>
-        <Grid2 xs={5}>
+      <div className={`${style.about__intro}`}>
+        <div className={`${style.about__intro_img}`}>
+          <img src={require("../../assets/about-us-img/Description_Img.png")} />
+        </div>
+
+        <div className={`${style.about__intro_text}`}>
+          <Typography variant="body1">{intro.par1}</Typography>
+          <Typography variant="body1">{intro.par2}</Typography>
+        </div>
+      </div>
+
+      <div className={`${style.about__confirm}`}>
+        <div className={`${style.about__confirm_title}`}>
+          <img src={require("../../assets/about-us-img/Star.png")} />
           <Typography
-            fontWeight={500}
-            variant="h6"
+            fontWeight={700}
+            variant="h4"
             color="#283493"
             textAlign="center"
-            marginBottom={1}
+            marginBottom={2}>
+            {intro.par3}</Typography>
+          <img src={require("../../assets/about-us-img/Star.png")} />
+        </div>
+
+        <Typography
+          color="#283493"
+          textAlign="center"
+          fontWeight={500}
+          className={`${style.about__confirm_info}`}
+        >
+          {`Trao đổi, giải đáp thắc mắc sau ngay cả sau khi sự kiện đã kết thúc`}
+        </Typography>
+      </div>
+
+      <div>
+        <div className={`${style.about__solution_title}`}>
+          <img src={require("../../assets/about-us-img/Solution_Icon_Left.png")} />
+          <Typography
+            fontWeight={700}
+            variant="h5"
+            color="#283493"
+            textAlign="center"
+            marginBottom={3}
           >
             Hỏi & Đáp sau hội thảo
           </Typography>
-          <Typography variant="body1">{solution.solution1}</Typography>
-        </Grid2>
-        <Divider orientation="vertical" flexItem />
-        <Grid2 xs={5}>
+          <img src={require("../../assets/about-us-img/Solution_Icon_Right.png")} />
+        </div>
+
+        <div className={`${style.about__QA}`}>
+          <div className={`${style.about__QA_description}`}>
+            <Typography variant="body1">{solution.QA_1}</Typography>
+            <Typography variant="body1">{solution.QA_2}</Typography>
+            <Typography variant="body1">{solution.QA_3}</Typography>
+          </div>
+
+          <div className={`${style.about__QA_img}`}>
+            <img src={require("../../assets/about-us-img/QA.png")} />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className={`${style.about__solution_title}`}>
+          <img src={require("../../assets/about-us-img/Solution_Icon_Left.png")} />
           <Typography
-            fontWeight={500}
-            variant="h6"
+            fontWeight={700}
+            variant="h5"
             color="#283493"
             textAlign="center"
-            marginBottom={1}
+            marginBottom={2}
           >
             Kết nối Sinh viên đến với các Chuyên gia
           </Typography>
-          <Typography variant="body1">{solution.solution2}</Typography>
-        </Grid2>
-      </Grid2>
+          <img src={require("../../assets/about-us-img/Solution_Icon_Right.png")} />
+        </div>
+
+
+        <div className={`${style.about__mentors}`}>
+          <div className={`${style.about__mentors_img}`}>
+            <img src={require("../../assets/about-us-img/Mentors.png")} />
+          </div>
+
+          <div className={`${style.about__mentors_description}`}>
+            <Typography variant="body1">{solution.Mentors_1}</Typography>
+            <Typography variant="body1">{solution.Mentors_2}</Typography>
+            <Typography variant="body1">{solution.Mentors_3}</Typography>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
